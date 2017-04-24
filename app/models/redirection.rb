c03bfffce72b92189a81569b35e9e61883b9dd29
tackeyy <mailto.takita.yusuke@gmail.com>
2017-04-25 00:43:49 +0900

    Redirect before to root url

---
+++ app/models/redirection.rb
@@ -0,0 +1,8 @@
+class Redirection < ActiveHash::Base
+  self.data = [
+    {id: 1, before:'/blog/posts', after:'/' },
+    {id: 2, before:'/blog/posts/2', after:'/' },
+    {id: 3, before:'/blog/posts/4', after:'/' },
+    {id: 4, before:'/blog/posts/5', after:'/' },
+  ]
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  get 'tags/show'
-
-  get 'categories/show'
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   root 'home#index'
 
+  Redirection.all.each do |redirection|
+    get redirection.before.to_s => redirect(redirection.after.to_s)
+  end
+
   concern :paginatable do
     get '(page/:page)', action: :index, on: :collection, as: ''
   end
 
-  resources :blog, only: [:show], concerns: :paginatable
-  resources :categories, only: [:index, :show]
-  resources :tags, only: [:show]
+  resources :blog, only: %i(show), concerns: :paginatable
+  resources :categories, only: %i(index show)
+  resources :tags, only: %i(show)
 end
