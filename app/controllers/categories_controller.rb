fd11463f4aa67ad2c1085988591910ff996c20dc
tackeyy <mailto.takita.yusuke@gmail.com>
2017-05-07 16:16:31 +0900

    Fix bug that is not filtered category and tag

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category_name = params[:id]
-    @posts = Post.tagged_with(params[:id]).page(params[:page]).decorate
+    @posts = Post.tagged_with(params[:id], on: :categories).page(params[:page]).decorate
     @categories = Post.tag_counts_on(:categories).decorate
     @tags = Post.tag_counts_on(:tags).decorate
   end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   def show
     @tag_name = params[:id]
-    @posts = Post.tagged_with(params[:id]).page(params[:page]).decorate
+    @posts = Post.tagged_with(params[:id], on: :tags).page(params[:page]).decorate
     @categories = Post.tag_counts_on(:categories).decorate
     @tags = Post.tag_counts_on(:tags).decorate
   end
