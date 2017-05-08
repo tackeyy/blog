341621bb1f9c70cab727004aa8536b592fc8cd5e
tackeyy <mailto.takita.yusuke@gmail.com>
2017-05-08 20:35:20 +0900

    Add path to sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,7 +4,16 @@ SitemapGenerator::Sitemap.default_host = Settings.url
 SitemapGenerator::Sitemap.create do
   add root_path
   add categories_path
+
   Post.shipped.each do |post|
     add blog_path(post.friendly_id || post.id), lastmod: post.updated_at
+  end
+
+  Post.tag_counts_on(:categories).each do |category|
+    add category_path(category.name)
+  end
+
+  Post.tag_counts_on(:tags).each do |tag|
+    add tag_path(tag.name)
   end
 end
