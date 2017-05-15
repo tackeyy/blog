1274a150a20c919d1702ea9da71b5d1c690f8245
tackeyy <mailto.takita.yusuke@gmail.com>
2017-05-15 18:26:50 +0900

    Add redirect url

--- app/models/redirection.rb
+++ app/models/redirection.rb
@@ -1,8 +1,9 @@
 class Redirection < ActiveHash::Base
   self.data = [
-    {id: 1, before:'/blog/posts', after:'/' },
+    {id: 1, before:'/blog/posts',   after:'/' },
     {id: 2, before:'/blog/posts/2', after:'/' },
-    {id: 3, before:'/blog/posts/4', after:'/' },
-    {id: 4, before:'/blog/posts/5', after:'/' },
+    {id: 3, before:'/blog/posts/3', after:'/' },
+    {id: 4, before:'/blog/posts/4', after:'/' },
+    {id: 5, before:'/blog/posts/5', after:'/' },
   ]
 end
