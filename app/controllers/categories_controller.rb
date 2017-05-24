208644b163f9872386389cb8293961ec3e8b94b2
tackeyy <mailto.takita.yusuke@gmail.com>
2017-05-24 15:18:19 +0900

    Fix meta tags

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,19 @@
 class CategoriesController < ApplicationController
+  before_action :set_categories
+
   def index
-    @categories = Post.tag_counts_on(:categories).decorate
     @tags = Post.tag_counts_on(:tags).decorate
   end
 
   def show
     @category_name = params[:id]
     @posts = Post.tagged_with(params[:id], on: :categories).page(params[:page]).decorate
-    @categories = Post.tag_counts_on(:categories).decorate
     @tags = Post.tag_counts_on(:tags).decorate
+  end
+
+  private
+
+  def set_categories
+    @categories = Post.tag_counts_on(:categories).decorate
   end
 end
