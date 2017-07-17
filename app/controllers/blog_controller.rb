2dad5388bbf5449133255fc32d5e80e65b55ade7
tackeyy <mailto.takita.yusuke@gmail.com>
2017-07-17 12:16:41 +0900

    Show articles on side nav

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,8 +1,8 @@
 class BlogController < ApplicationController
+  before_action :set_posts, only: [:index, :show]
   before_action :set_post, only: [:show]
 
   def index
-    @posts = Post.shipped.order(created_at: :desc).page(params[:page]).decorate
     @categories = Post.tag_counts_on(:categories).decorate
     @tags = Post.tag_counts_on(:tags).decorate
   end
@@ -13,6 +13,10 @@ class BlogController < ApplicationController
   end
 
   private
+
+  def set_posts
+    @posts = Post.shipped.order(created_at: :desc).page(params[:page]).decorate
+  end
 
   def set_post
     @post = Post.friendly.find(params[:id]).decorate || Post.find(params[:id]).decorate
