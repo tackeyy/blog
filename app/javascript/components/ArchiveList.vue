bd7fda3ba33d11c06ffcdca762e41b5ab5dd982b
tackeyy <mailto.takita.yusuke@gmail.com>
2017-08-12 15:20:12 +0900

    Show dummy archives on side nav

--- app/javascript/components/ArchiveList.vue
+++ app/javascript/components/ArchiveList.vue
@@ -1,27 +1,52 @@
 <template>
   <section>
     <header>
-      <h3>アーカイブ</h3>
+      <h3>{{ label }}</h3>
     </header>
     <hr>
-    <ul>
-      <li class="hoge">hoge</li>
-      <li>huga</li>
+    <ul v-for="year in this.years" class="archive-list-ul">
+      <li @click="toggle">
+        <a :href="year.path">{{ year.name }}({{ year.total }})</a>
+      </li>
+      <ul v-for="month in year.months">
+        <li @click="toggle">
+          <a>{{ month.name }}({{ month.total }})</a>
+        </li>
+      </ul>
     </ul>
   </section>
 </template>
 
 <script>
-module.exports = {
+export default {
   data () {
     return {
+      label: 'アーカイブ',
+      years: this.getArchives()
     }
-  }
+  },
+  methods: {
+    toggle () {
+      if (this.isFolder) {
+        this.open = !this.open
+      }
+    },
+    getArchives () {
+      const node = document.getElementById('archive-list')
+      return JSON.parse(node.getAttribute('data-archives'))
+    }
+  },
+  computed: {
+    isFolder: function () {
+      return this.archives &&
+        this.archives.length
+    }
+  },
 }
 </script>
 
 <style lans="scss" scoped>
-.hoge {
-  color: red;
+.archive-list-ul {
+  line-height: 1.5em;
 }
 </style>
--- app/javascript/packs/ArchiveList.js
+++ app/javascript/packs/ArchiveList.js
@@ -3,8 +3,10 @@ import ArchiveList from "../components/ArchiveList.vue"
 
 document.addEventListener("DOMContentLoaded", () => {
   new Vue({
-    el: "#archiveList",
-    template: "<archiveList/>",
-    components: { ArchiveList }
+    el: "ArchiveList",
+    template: "<ArchiveList/>",
+    components: {
+      ArchiveList
+    }
   })
 })
