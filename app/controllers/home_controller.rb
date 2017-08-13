03d52a0be885f49aeadce9cacb57c47259fcfaaa
tackeyy <mailto.takita.yusuke@gmail.com>
2017-08-13 19:45:49 +0900

    Show archive posts on sidenav (#38)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @posts      = Post.shipped.order(created_at: :desc).page(params[:page]).decorate
+    posts       = Post.shipped.order(created_at: :desc)
+    @posts      = posts.page(params[:page]).decorate
+    @archives   = Post.archive_list(posts: posts)
     @categories = Post.tag_counts_on(:categories).decorate
     @tags       = Post.tag_counts_on(:tags).decorate
   end
